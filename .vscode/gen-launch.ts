ed8926ff468721fb8afbbb353967e1bce68193e4
Markus Peitl <office@markuspeitl.com>
2024-08-15 22:39:35 +0200

    - Implemented special config generation keys parametrize, parametrize-keys - Implemented paramerter inflate termination with adding a '$' token last after param key - Refactored special key parsing into own function - Added cloning the default dicts before modification to prevent side effects up the chain - Added passing an initial config to function to use a predefined launch.json and add configurations to that

--- .vscode/gen-launch.ts
+++ .vscode/gen-launch.ts
@@ -2,12 +2,6 @@ import { arrayifyFilter, inflateTemplateVars, loadDataAsync, loadDataSources } f
 import path from "path";
 import * as fs from 'fs';
 
-
-const launchFilePath: string = path.join(__dirname, './launch.json');
-const targetFilePath: string = path.join(__dirname, './launch-generated.json');
-const generatorFilePath: string = path.join(__dirname, './launch-configs-template.json');
-
-
 export async function getConfigurationArrayFrom(jsonFilePath: string): Promise<any[]> {
     const loadedJson: any = await loadDataAsync(jsonFilePath);
     return loadedJson?.configurations || [];
@@ -23,7 +17,7 @@ export async function getConfigurations(jsonFilePaths: string[]): Promise<any[]>
 export function inflateTemplatedValuesIn(targetDict: any, valuesDict: any): any {
 
     if (typeof targetDict === 'string') {
-        return inflateTemplateVars(targetDict, valuesDict);
+        return inflateTemplateVars(targetDict, valuesDict, undefined, undefined, '[$]?');
     }
 
     if (typeof targetDict !== 'object') {
@@ -39,11 +33,80 @@ export function inflateTemplatedValuesIn(targetDict: any, valuesDict: any): any 
     return targetDict;
 }
 
+const specialKeys: string[] = [
+    'default',
+    'extend',
+    'parametrize',
+    'parametrize-keys'
+];
+
+export function assignMergeDefaults(target: any, src: any): any {
+
+    if (!target) {
+        target = {};
+    }
+
+    for (const key in src) {
+        if (key === 'params') {
+            target.params = Object.assign(target.params || {}, src.params);
+        }
+        else if (src[ key ]) {
+            target[ key ] = src[ key ];
+        }
+    }
+
+    return target;
+}
+
+export function parserSpecialKeyDict(content: any, key: string, currentDefault: any = {}): null | any[] {
+    if (key === 'default') {
+        currentDefault = assignMergeDefaults(currentDefault, content);
+        return null;
+    }
+    if (key === 'parametrize' || key === 'parametrize-keys') {
+
+        const subLaunchConfigs: any[] = [];
+
+        for (const parameters of content) {
+
+            let virtualOverrideEntry: any = {};
+
+            if (typeof parameters === 'string') {
+                virtualOverrideEntry = {
+                    params: {
+                        key: parameters
+                    }
+                };
+            }
+            else {
+                virtualOverrideEntry = {
+                    params: parameters
+                };
+            }
+
+            let key = null;
+            if (virtualOverrideEntry.key) {
+                key = parameters.key;
+            }
+            const branchSubLaunchConfigs: any[] = getLaunchConfigsFromGenJson(virtualOverrideEntry, key, currentDefault);
+            subLaunchConfigs.push(...branchSubLaunchConfigs);
+        }
+
+        return subLaunchConfigs;
+    }
+    return null;
+}
+
+export function cloneDict(dict) {
+    return JSON.parse(JSON.stringify(dict));
+}
+
 export function getLaunchConfigsFromGenJson(genJson: any, currentKey: string | null = null, parentDefault: any = {}): any[] {
 
     if (genJson.params || !genJson.default) {
 
-        const merged = Object.assign({}, parentDefault, genJson);
+        let merged = assignMergeDefaults({}, parentDefault);
+        merged = assignMergeDefaults(merged, genJson);
 
         if (!merged.params) {
             merged.params = {};
@@ -60,12 +123,16 @@ export function getLaunchConfigsFromGenJson(genJson: any, currentKey: string | n
             params: paramsDict
         };
 
+        const mergedClone = cloneDict(merged);
+
+        const inflatedValues = inflateTemplatedValuesIn(mergedClone, paramScopedDict);
+
         return [
-            inflateTemplatedValuesIn(merged, paramScopedDict)
+            inflatedValues
         ];
     }
 
-    let currentDefault: any = Object.assign({}, parentDefault || {});
+    let currentDefault = cloneDict(parentDefault);
 
     const subLaunchConfigs: any[] = [];
 
@@ -73,11 +140,13 @@ export function getLaunchConfigsFromGenJson(genJson: any, currentKey: string | n
 
         const currentItemValue: any = genJson[ key ];
 
-        if (key === 'default') {
-            currentDefault = Object.assign(currentDefault, currentItemValue);
+        let handleItemFn: Function = getLaunchConfigsFromGenJson;
+        if (specialKeys.includes(key)) {
+            handleItemFn = parserSpecialKeyDict;
         }
-        if (key !== 'default' && key !== 'extend') {
-            const branchSubLaunchConfigs: any[] = getLaunchConfigsFromGenJson(currentItemValue, key, currentDefault);
+
+        const branchSubLaunchConfigs: any[] | null = handleItemFn(currentItemValue, key, currentDefault);
+        if (branchSubLaunchConfigs) {
             subLaunchConfigs.push(...branchSubLaunchConfigs);
         }
     }
@@ -108,11 +177,24 @@ export async function inflateGenerateJson(genJson: any, filePath: string): Promi
 }
 
 
-export async function generateLaunchConfig(generatorJsonPath: string, targetFilePath: string): Promise<any> {
+export async function generateLaunchConfig(
+    generatorJsonPath: string,
+    targetFilePath: string,
+    baseLaunchConfig: any = null,
+): Promise<any> {
 
     const loadedGenJson: any = await loadDataAsync(generatorJsonPath);
 
-    const inflatedLaunchJson: any = await inflateGenerateJson(loadedGenJson, generatorJsonPath);
+    let inflatedLaunchJson: any = await inflateGenerateJson(loadedGenJson, generatorJsonPath);
+
+    if (baseLaunchConfig) {
+        const inflatedConfigurations = inflatedLaunchJson.configurations || [];
+        inflatedLaunchJson = baseLaunchConfig;
+        if (!inflatedLaunchJson.configurations) {
+            inflatedLaunchJson.configurations = [];
+        }
+        inflatedLaunchJson.configurations.push(...inflatedConfigurations);
+    }
 
     const jsonString = JSON.stringify(inflatedLaunchJson, null, 4);
     await fs.promises.writeFile(targetFilePath, jsonString);
@@ -120,4 +202,17 @@ export async function generateLaunchConfig(generatorJsonPath: string, targetFile
     return inflatedLaunchJson;
 }
 
-generateLaunchConfig(generatorFilePath, targetFilePath);
+
+const launchFilePath: string = path.join(__dirname, './launch.json');
+//const targetFilePath: string = path.join(__dirname, './launch-generated.json');
+const generatorFilePath: string = path.join(__dirname, './launch-configs-template.json');
+const targetFilePath: string = path.join(__dirname, './launch.json');
+
+
+generateLaunchConfig(
+    generatorFilePath,
+    targetFilePath,
+    {
+        'version': '0.2.0'
+    }
+);
